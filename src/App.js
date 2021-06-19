45a55d3254a2e606f5955199cb314ab81ef3bc51
Cristian Rojas <credrope@hotmail.com>
2021-06-19 13:24:33 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+
+  }
+
+  increment(){
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrement(){
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  reset(){
+    this.setState({
+      counter: 0
+    })
+  }
+  render(){
+    return (
+      <main>
+        <div className="container">
+          <h1>Counter</h1>
+          <span className={this.state.counter > 0 ? 'text-increment': this.state.counter < 0? 'text-decrement': 'text-reset'}>{this.state.counter}</span>
+          <div className="container-buttons">
+            <button onClick={this.decrement}>Decrement</button>
+            <button onClick={this.reset}>Reset</button>
+            <button onClick={this.increment}>Increment</button>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
